915cadb06a3c152f869fad21378620ce106334de
yousefsaad12 <saadrslan95@gmail.com>
2026-08-16 01:26:35 +0300

    handle slash outside quouts

---
+++ src/tokenizer.rs
@@ -0,0 +1,46 @@
+pub fn tokenize(input: &str) -> Vec<String> {
+    let mut args = Vec::new();
+    let mut current_arg = String::new();
+    let mut in_single_quotes = false;
+    let mut in_double_quotes = false;
+    let mut arg_start = false;
+
+    let mut chars = input.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        match c {
+            '\'' if !in_double_quotes => {
+                in_single_quotes = !in_single_quotes;
+                arg_start = true;
+            }
+            '"' if !in_single_quotes => {
+                in_double_quotes = !in_double_quotes;
+                arg_start = true;
+            }
+            '\\' if !in_single_quotes && !in_double_quotes => {
+                // Outside quotes: backslash escapes the very next char literally
+                if let Some(next_c) = chars.next() {
+                    current_arg.push(next_c);
+                    arg_start = true;
+                }
+                // if there's no next char (trailing backslash), just drop it
+            }
+            c if c.is_whitespace() && !in_single_quotes && !in_double_quotes => {
+                if arg_start {
+                    args.push(std::mem::take(&mut current_arg));
+                    arg_start = false;
+                }
+            }
+            _ => {
+                current_arg.push(c);
+                arg_start = true;
+            }
+        }
+    }
+
+    if arg_start {
+        args.push(current_arg);
+    }
+
+    args
+}
